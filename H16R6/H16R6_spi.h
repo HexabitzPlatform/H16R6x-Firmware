1b5f271f59996e6575b790f5d7ff776febc7c984
Mohamad Khatib <116002861+mohamad1999khatib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@users.noreply.github.com>
2024-04-21 22:16:47 +0300

    Adding  SPI files.

---
+++ H16R6/H16R6_spi.h
@@ -0,0 +1,64 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    spi.h
+  * @brief   This file contains all the function prototypes for
+  *          the spi.c file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __SPI_H__
+#define __SPI_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g0xx_hal.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+extern SPI_HandleTypeDef hspi1;
+
+/* USER CODE BEGIN Private defines */
+/* SPI type define */
+typedef SPI_HandleTypeDef SPI_HANDLE;
+
+/* USER CODE BEGIN ET */
+typedef enum{
+	STATUS_OK = 0,
+	STATUS_INV,
+	STATUS_TMOUT,
+	STATUS_ERR=255
+}Status_TypeDef;
+/* SPI port definitions */
+#define TIM_OUT_10MS         		10u
+
+/* USER CODE END Private defines */
+
+void MX_SPI1_Init(void);
+Status_TypeDef SendSPI(SPI_HANDLE *xPort, uint8_t pData[], uint16_t Size);
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SPI_H__ */
+
