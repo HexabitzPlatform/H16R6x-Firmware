8ea5b803100c379cf36aff4596dc72dc8b9a213a
Mohamad Khatib <116002861+mohamad1999khatib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@users.noreply.github.com>
2024-04-22 13:33:44 +0300

    Testing All APIs -->Work

--- User/main.c
+++ User/main.c
@@ -29,6 +29,25 @@ void UserTask(void *argument){
 
 	// put your code here, to run repeatedly.
 	while(1){
+		LEDMatrix_SetColor(0, 50, 100, 150, 10);
+		LEDMatrix_SetColor(2, 0, 255, 0, 10);
+		Delay_ms(1000);
+		LEDMatrix_SetLedOff(2);
+		Delay_ms(1000);
+		LEDMatrix_SetLedOn(2, 30);
+		Delay_ms(1000);
+		LEDMatrix_SetAllColor(0, 0, 255, 20);
+		Delay_ms(1000);
+		LEDMatrix_SetAllLedOff();
+		Delay_ms(1000);
+		LEDMatrix_SetAllLedOn(5);
+		Delay_ms(1000);
+		LEDMatrix_SetRGB(1, 0x0A010F, 16);
+		Delay_ms(1000);
+		LEDMatrix_SetAllRGB(0xF00F01, 10);
+		Delay_ms(1000);
+
+
 
 	}
 }
