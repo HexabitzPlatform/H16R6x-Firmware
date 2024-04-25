2b90fa47f76fd66e72de89a4d40a54ccf0dcff9d
Mohamad Khatib <116002861+mohamad1999khatib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@users.noreply.github.com>
2024-04-25 11:15:45 +0300

    Testing All Mesages

--- User/main.c
+++ User/main.c
@@ -30,24 +30,23 @@ void UserTask(void *argument){
 	// put your code here, to run repeatedly.
 	while(1){
 
-		LEDMatrix_SetRGB(4, 50, 100, 150, 10);
-		LEDMatrix_SetRGB(2, 0, 255, 0, 10);
-		Delay_ms(1000);
-		LEDMatrix_SetLedOff(2);
-		Delay_ms(1000);
-		LEDMatrix_SetLedOn(2, 30);
-		Delay_ms(1000);
-		LEDMatrix_SetAllRGB(0, 0, 255, 20);
-		Delay_ms(1000);
-		LEDMatrix_SetAllLedOff();
-		Delay_ms(1000);
-		LEDMatrix_SetAllLedOn(5);
-		Delay_ms(1000);
-		LEDMatrix_SetColor(1,LIGHTBLUE, 16);
-		Delay_ms(1000);
-		LEDMatrix_SetAllColor(WHITE, 10);
-		Delay_ms(1000);
-
+//		LEDMatrix_SetRGB(4, 50, 100, 150, 10);
+//		LEDMatrix_SetRGB(2, 0, 255, 0, 10);
+//		Delay_ms(1000);
+//		LEDMatrix_SetLedOff(2);
+//		Delay_ms(1000);
+//		LEDMatrix_SetLedOn(2, 30);
+//		Delay_ms(1000);
+//		LEDMatrix_SetAllRGB(0, 0, 255, 20);
+//		Delay_ms(1000);
+//		LEDMatrix_SetAllLedOff();
+//		Delay_ms(1000);
+//		LEDMatrix_SetAllLedOn(5);
+//		Delay_ms(1000);
+//		LEDMatrix_SetColor(1,LIGHTBLUE, 16);
+//		Delay_ms(1000);
+//		LEDMatrix_SetAllColor(WHITE, 10);
+//		Delay_ms(1000);
 
 
 	}
