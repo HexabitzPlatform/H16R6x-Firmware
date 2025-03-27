75d52861ca0ffe9ecfaef1bfa5b893c0ddb92d71
Mahmood_Reda <mr4141705@gmail.com>
2025-03-27 17:31:55 +0300

    add ((GetModuleParameter)) empty function .

--- H16R6/H16R6.c
+++ H16R6/H16R6.c
@@ -33,7 +33,7 @@ uint8_t OldeColorR[65]={0};
 uint8_t OldeColorG[65]={0};
 uint8_t OldeColorB[65]={0};
 /* Module exported parameters ------------------------------------------------*/
-module_param_t modParam[NUM_MODULE_PARAMS] ={{.paramPtr = NULL, .paramFormat =FMT_FLOAT, .paramName =""}};
+module_param_t modParam[NUM_MODULE_PARAMS] ={0};
 
 /* Private variables ---------------------------------------------------------*/
 
@@ -485,6 +485,27 @@ void Module_Peripheral_Init(void){
 
 	/* Create module special task (if needed) */
 
+}
+
+/***************************************************************************/
+/* This functions is useful only for input (sensors) modules.
+ * @brief: Samples a module parameter value based on parameter index.
+ * @param paramIndex: Index of the parameter (1-based index).
+ * @param value: Pointer to store the sampled float value.
+ * @retval: Module_Status indicating success or failure.
+ */
+Module_Status GetModuleParameter(uint8_t paramIndex,float *value){
+	Module_Status status =BOS_OK;
+
+	switch(paramIndex){
+
+		/* Invalid parameter index */
+		default:
+			status =BOS_ERR_WrongParam;
+			break;
+	}
+
+	return status;
 }
 
 /*-----------------------------------------------------------*/
